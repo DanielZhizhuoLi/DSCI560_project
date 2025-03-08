695b3fd89c45bf70af91bff2247c41a2bec498b5
Tim Lee <tim900701@gmail.com>
2025-03-08 14:21:24 -0800

    upload word2vec
    *update word2vec.py
    * upload word2vec different dimension vectors

--- Lab8/scripts/word2vec.py
+++ Lab8/scripts/word2vec.py
@@ -6,20 +6,17 @@ from nltk.corpus import stopwords
 import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
-import json
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
 from sklearn.metrics.pairwise import euclidean_distances
 import matplotlib.pyplot as plt
 import numpy as np
-from collections import defaultdict 
 
 # download stopwords 
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
-
 
 
 DB_HOST = "localhost"
@@ -52,13 +49,15 @@ tokens = tokenization(df)
 print(f"total tokens in all the data: {len(tokens)}")
 
 
-def vectorization(tokens):
-	model = gensim.models.Word2Vec(tokens, min_count= 1, vector_size = 50, window = 5)
+def vectorization(tokens, dim):
+	model = gensim.models.Word2Vec(tokens, min_count= 1, vector_size = dim, window = 5)
 	words = list(model.wv.index_to_key)
 	word_vectors = {word: model.wv[word] for word in words}
 	return words, word_vectors
 
-words, vectors = vectorization(tokens)
+dim_25_words, dim_25_vectors = vectorization(tokens, 25)
+dim_50_words, dim_50_vectors = vectorization(tokens, 50)
+dim_100_words, dim_100_vectors = vectorization(tokens, 100)
 
 def find_k(word_vectors):
 	vector_values  = np.array(list(word_vectors.values()))
@@ -71,33 +70,40 @@ def find_k(word_vectors):
 
 def plot_elbow(vectors):
 	wcss = find_k(vectors)
-	# found k = 6 is optimal 
 	plt.figure(figsize=(8,5))
 	plt.plot(range(1, 16), wcss, marker= 'o', linestyle='--')
 	plt.xlabel("Number of Clusters (K)")
 	plt.ylabel("WCSS")
-	plt.savefig("elbow_plot.png")
+	plt.title("dim 100 elbow method")
+	plt.savefig(f"../data/word2vec/dim_100_elbow_plot.png")
 	plt.close()
 	return 
-#plot_elbow()
+# plot_elbow(dim_25_vectors)
+# plot_elbow(dim_50_vectors)
+plot_elbow(dim_100_vectors)
 
-def get_bins(vectors):
+def get_bins(vectors, n_clusters):
 	words = list(vectors.keys())
 	vector_values  = np.array(list(vectors.values()))
-	kmeans = KMeans(n_clusters = 6, random_state = 42)
+	kmeans = KMeans(n_clusters = n_clusters, random_state = 42)
 	kmeans.fit(vector_values)
 	labels = kmeans.labels_
-	bins = {i: [] for i in range(6)}
+	bins = {i: [] for i in range(n_clusters)}
 	for word, label in zip(words, labels):
 		bins[label].append(word)
 	return bins
 
-bins = get_bins(vectors)
+# dim 25 get 4 bins
+dim_25_bins = get_bins(dim_25_vectors, 4)
+# dim 50 get 6 bins
+dim_50_bins = get_bins(dim_50_vectors, 6)
+# dim 100 get 7 bins
+dim_100_bins = get_bins(dim_100_vectors, 7)
 
-def bag_of_word(tokens, bins):
+def bag_of_word(tokens, bins, n):
 	bow = []
 	for post in tokens:
-		bin_count = [0] * 6
+		bin_count = [0] * n
 		for word in post:
 			for bin_id, bin_words in bins.items():
 				if word in bin_words:
@@ -106,7 +112,9 @@ def bag_of_word(tokens, bins):
 		bow.append(bin_count)
 	return bow
 
-bow = bag_of_word(tokens, bins)
+dim_25_bow = bag_of_word(tokens, dim_25_bins, 4)
+dim_50_bow = bag_of_word(tokens, dim_50_bins, 6)
+dim_100_bow = bag_of_word(tokens, dim_100_bins, 7)
 
 def normalization(bow):
 	norms_bow = []
@@ -118,10 +126,18 @@ def normalization(bow):
 			norms_bow.append([0]* len(bow_count))
 	return norms_bow
 
-norms_bow = normalization(bow)
+dim_25_norms_bow = normalization(dim_25_bow)
+dim_50_norms_bow = normalization(dim_50_bow)
+dim_100_norms_bow = normalization(dim_100_bow)
 
-def clustering(norms_bow):
-	kmeans = KMeans(n_clusters= 6, random_state = 42)
+
+# merge_df = pd.concat([df_bow_25, df_bow_50, df_bow_100], axis = 1)
+# merge_df.to_csv("../data/word2vec_vectors.csv", index=False)
+# print("sucessfully ouput as csv")
+
+
+def clustering(norms_bow, n_clusters):
+	kmeans = KMeans(n_clusters= n_clusters, random_state = 42)
 	kmeans.fit(norms_bow)
 	centroids = kmeans.cluster_centers_
 
@@ -129,21 +145,79 @@ def clustering(norms_bow):
 	closest_posts = np.argmin(distances, axis = 1)
 	return kmeans, centroids, closest_posts
 
-kmeans_model, centroids, closest_posts = clustering(norms_bow)
-print(centroids)
 pca = PCA(n_components= 2)
-reduced_pca = pca.fit_transform(norms_bow)
-reduced_centroids = pca.fit_transform(centroids)
+
+# dimension of 25 
+dim_25_kmeans_model, dim_25_centroids, dim_25_closest_posts = clustering(dim_25_norms_bow, 4)
+dim_25_reduced_pca = pca.fit_transform(dim_25_norms_bow)
+dim_25_reduced_centroids = pca.fit_transform(dim_25_centroids)
 
 plt.figure(figsize = (10,6))
-plt.scatter(reduced_pca[:, 0], reduced_pca[:, 1], c=kmeans_model.labels_, cmap='viridis', marker='.')
-plt.scatter(reduced_centroids[:, 0], reduced_centroids[:, 1], c='red', marker='x', s= 100, label= 'Centroids')
-for i, closest_post in enumerate(closest_posts):
-	plt.annotate(f'Post {closest_post +1}', (reduced_pca[closest_post, 0], reduced_pca[closest_post, 1]), textcoords="offset points", xytext=(0, 5), ha='center')
+plt.scatter(dim_25_reduced_pca[:, 0], dim_25_reduced_pca[:, 1], c=dim_25_kmeans_model.labels_, cmap='viridis', marker='.')
+plt.scatter(dim_25_reduced_centroids[:, 0], dim_25_reduced_centroids[:, 1], c='red', marker='x', s= 100, label= 'Centroids')
+for i, closest_post in enumerate(dim_25_closest_posts):
+	plt.annotate(f'Post {closest_post +1}', (dim_25_reduced_pca[closest_post, 0], dim_25_reduced_pca[closest_post, 1]), textcoords="offset points", xytext=(0, 5), ha='center')
 
 plt.xlabel("pca 1")
 plt.ylabel("pca 2")
 plt.title("Kmeans cluster with centroids and closest posts")
 plt.legend()
-plt.savefig("clustering.png")
+plt.savefig("../data/word2vec/dim_25_clustering.png")
 plt.close()
+print("dim 25 cluster is saved")
+
+
+# dimension of 50 
+dim_50_kmeans_model, dim_50_centroids, dim_50_closest_posts = clustering(dim_50_norms_bow, 6)
+dim_50_reduced_pca = pca.fit_transform(dim_50_norms_bow)
+dim_50_reduced_centroids = pca.fit_transform(dim_50_centroids)
+
+plt.figure(figsize = (10,6))
+plt.scatter(dim_50_reduced_pca[:, 0], dim_50_reduced_pca[:, 1], c=dim_50_kmeans_model.labels_, cmap='viridis', marker='.')
+plt.scatter(dim_50_reduced_centroids[:, 0], dim_50_reduced_centroids[:, 1], c='red', marker='x', s= 100, label= 'Centroids')
+for i, closest_post in enumerate(dim_50_closest_posts):
+	plt.annotate(f'Post {closest_post +1}', (dim_50_reduced_pca[closest_post, 0], dim_50_reduced_pca[closest_post, 1]), textcoords="offset points", xytext=(0, 5), ha='center')
+
+plt.xlabel("pca 1")
+plt.ylabel("pca 2")
+plt.title("Kmeans cluster with centroids and closest posts")
+plt.legend()
+plt.savefig("../data/word2vec/dim_50_clustering.png")
+plt.close()
+print("dim 50 cluster is saved")
+
+
+
+# dimension of 100
+dim_100_kmeans_model, dim_100_centroids, dim_100_closest_posts = clustering(dim_100_norms_bow, 7)
+dim_100_reduced_pca = pca.fit_transform(dim_100_norms_bow)
+dim_100_reduced_centroids = pca.fit_transform(dim_100_centroids)
+
+plt.figure(figsize = (10,6))
+plt.scatter(dim_100_reduced_pca[:, 0], dim_100_reduced_pca[:, 1], c=dim_100_kmeans_model.labels_, cmap='viridis', marker='.')
+plt.scatter(dim_100_reduced_centroids[:, 0], dim_100_reduced_centroids[:, 1], c='red', marker='x', s= 100, label= 'Centroids')
+for i, closest_post in enumerate(dim_100_closest_posts):
+	plt.annotate(f'Post {closest_post +1}', (dim_100_reduced_pca[closest_post, 0], dim_100_reduced_pca[closest_post, 1]), textcoords="offset points", xytext=(0, 5), ha='center')
+
+plt.xlabel("pca 1")
+plt.ylabel("pca 2")
+plt.title("Kmeans cluster with centroids and closest posts")
+plt.legend()
+plt.savefig("../data/word2vec/dim_100_clustering.png")
+plt.close()
+print("dim 100 cluster is saved")
+
+
+
+def calculate_silhouette_score(vec, labels):
+	score = silhouette_score(vec, labels)
+	return score
+
+silhouette_25 = calculate_silhouette_score(dim_25_norms_bow, dim_25_kmeans_model.labels_)
+silhouette_50 = calculate_silhouette_score(dim_50_norms_bow, dim_50_kmeans_model.labels_)
+silhouette_100 = calculate_silhouette_score(dim_100_norms_bow, dim_100_kmeans_model.labels_)
+
+
+print(f"Silhouette score for word2vec 25-dimension vectors: {silhouette_25}")
+print(f"Silhouette score for word2vec 50-dimension vectors: {silhouette_50}")
+print(f"Silhouette score for word2vec 100-dimension vectors: {silhouette_100}")
